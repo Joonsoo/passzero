4f04a48b12b7a5ffc7e7ea045cf359edc02c80c5
Joonsoo Jeon <giyeok7@gmail.com>
2017-03-12 18:50:49 +0900

    new SortedList implemented

--- src/main/scala/com/giyeok/passzero/ui/swt/PasswordListUI.scala
+++ src/main/scala/com/giyeok/passzero/ui/swt/PasswordListUI.scala
@@ -17,10 +17,12 @@ import com.giyeok.passzero.storage.Path
 import com.giyeok.passzero.ui.Config
 import com.giyeok.passzero.utils.FutureStream
 import org.eclipse.swt.SWT
+import org.eclipse.swt.custom.ScrolledComposite
 import org.eclipse.swt.events.SelectionEvent
 import org.eclipse.swt.events.SelectionListener
 import org.eclipse.swt.layout.GridLayout
 import org.eclipse.swt.widgets
+import org.eclipse.swt.widgets.Canvas
 import org.eclipse.swt.widgets.Composite
 import org.eclipse.swt.widgets.Display
 import org.eclipse.swt.widgets.Shell
@@ -60,7 +62,7 @@ class PasswordListUI(val shell: Shell, parent: MainUI, style: Int, session: Sess
     newSheetBtn.addSelectionListener(new SelectionListener {
         def widgetSelected(e: SelectionEvent): Unit = {
             directoryList.selectedItem foreach { pair =>
-                val directory = pair._1
+                val directory = pair._1.data
                 passwordMgr.sheet.createSheet(directory, s"amazon.com ${Math.abs(Random.nextInt())}", SheetType.Login)
             }
         }
@@ -84,19 +86,17 @@ class PasswordListUI(val shell: Shell, parent: MainUI, style: Int, session: Sess
         def widgetDefaultSelected(e: SelectionEvent): Unit = {}
     })
 
-    private val directoryList = new SortedList[Password.Directory](getDisplay, this, SWT.BORDER) {
-        override def >(a: Directory, b: Directory): Boolean = a.name > b.name
-        override def repr(item: Directory): String = item.name
-        override def selected(item: Directory, index: Int): Unit = setSelectedDirectory(item)
-    }
-    directoryList.listWidget.setLayoutData(fillAll())
+    private val directoryList = new SortedList[TextSortedListItem[Password.Directory]](getDisplay, this, SWT.BORDER)
+    directoryList.setLayoutData(fillAll())
+    directoryList.addSelectListener({ selectedOpt =>
+        setSelectedDirectory(selectedOpt map { _._1.data })
+    })
 
-    private val sheetList = new SortedList[Password.Sheet](getDisplay, this, SWT.BORDER) {
-        override def >(a: Sheet, b: Sheet): Boolean = a.name > b.name
-        override def repr(item: Sheet): String = item.name
-        override def selected(item: Sheet, index: Int): Unit = setSelectedSheet(item)
-    }
-    sheetList.listWidget.setLayoutData(fillAll())
+    private val sheetList = new SortedList[TextSortedListItem[Password.Sheet]](getDisplay, this, SWT.BORDER)
+    sheetList.setLayoutData(fillAll())
+    sheetList.addSelectListener({ selectedOpt =>
+        setSelectedSheet(selectedOpt map { _._1.data })
+    })
 
     private val sheetView = new SheetContentView(this, SWT.BORDER)
     sheetView.setLayoutData(fillAll())
@@ -105,7 +105,9 @@ class PasswordListUI(val shell: Shell, parent: MainUI, style: Int, session: Sess
         session.ensureInitialized() onComplete {
             case Success(_) =>
                 getDisplay.syncExec(() =>
-                    directoryList.setSource(passwordMgr.directory.directoryList()))
+                    directoryList.setSource(passwordMgr.directory.directoryList() map { directories =>
+                        directories map { directory => TextSortedListItem(directory, directory.name) }
+                    }))
             case Failure(reason) =>
                 reason.printStackTrace()
                 getDisplay.syncExec(() => showMessage(reason.getMessage))
@@ -113,89 +115,35 @@ class PasswordListUI(val shell: Shell, parent: MainUI, style: Int, session: Sess
     }
     start()
 
-    private def setSelectedDirectory(directory: Password.Directory): Unit = {
-        sheetList.setSource(passwordMgr.sheet.sheetList(directory))
-        sheetView.clearAll()
-    }
-
-    private def setSelectedSheet(sheet: Password.Sheet): Unit = {
-        passwordMgr.sheetDetail.sheetDetail(sheet) foreach { detailOpt =>
-            getDisplay.syncExec(() => {
-                detailOpt match {
-                    case Some(detail) =>
-                        sheetView.setDetail(detail)
-                    case None =>
-                        sheetView.setError()
-                }
-            })
+    private def setSelectedDirectory(directoryOpt: Option[Password.Directory]): Unit = {
+        directoryOpt match {
+            case Some(directory) =>
+                sheetList.setSource(passwordMgr.sheet.sheetList(directory) map { sheets =>
+                    sheets map { sheet => TextSortedListItem(sheet, sheet.name) }
+                })
+                sheetView.clearAll()
+            case None =>
+                sheetView.clearAll()
         }
     }
-}
 
-abstract class SortedList[T](display: Display, parent: Composite, style: Int) {
-    val listWidget = new widgets.List(parent, style)
-    private var items = Seq[T]()
-    private var _selectedItem = Option.empty[(T, Int)]
-
-    def selectedItem: Option[(T, Int)] = _selectedItem
-
-    private var _sourceId: Long = -1
-    private val sourceIdCounter = new AtomicLong(0)
-    private def newSourceId(): Long = {
-        val newId = sourceIdCounter.getAndIncrement()
-        this.synchronized { _sourceId = newId }
-        newId
-    }
-
-    listWidget.addSelectionListener(new SelectionListener {
-        override def widgetSelected(e: SelectionEvent): Unit = {
-            val index = listWidget.getSelectionIndex
-            if (0 <= index && index < items.length) {
-                _selectedItem = Some(items(index), index)
-                selected(items(index), index)
-            }
-        }
-
-        override def widgetDefaultSelected(e: SelectionEvent): Unit = {}
-    })
-
-    def clear(): Unit = {
-        listWidget.removeAll()
-        items = Seq()
-    }
-
-    def setSource(stream: FutureStream[Seq[T]]): Unit = {
-        clear()
-        listWidget.setEnabled(false)
-        val currentSourceId = newSourceId()
-        stream foreach { (page, tail) =>
-            this.synchronized {
-                if (this._sourceId == currentSourceId) {
-                    page foreach { item =>
-                        val index = items.zipWithIndex find { p => >(p._1, item) } map {
-                            _._2
-                        } getOrElse items.length
-                        val (init, tail) = items.splitAt(index)
-                        val newList: Seq[T] = init ++ (item +: tail)
-                        display.syncExec(() => {
-                            listWidget.add(repr(item), index)
-                        })
-                        items = newList
-                        newList
-                    }
-                    if (tail.isEmpty) display.syncExec(() => {
-                        listWidget.setEnabled(true)
+    private def setSelectedSheet(sheetOpt: Option[Password.Sheet]): Unit = {
+        sheetOpt match {
+            case Some(sheet) =>
+                passwordMgr.sheetDetail.sheetDetail(sheet) foreach { detailOpt =>
+                    getDisplay.syncExec(() => {
+                        detailOpt match {
+                            case Some(detail) =>
+                                sheetView.setDetail(detail)
+                            case None =>
+                                sheetView.setError()
+                        }
                     })
-                } else {
-                    // tail.cancel()
                 }
-            }
+            case None =>
+                sheetView.clearAll()
         }
     }
-
-    def >(a: T, b: T): Boolean // = a > b
-    def repr(item: T): String // = item.name
-    def selected(item: T, index: Int): Unit
 }
 
 class SheetContentView(parent: Composite, style: Int) extends Composite(parent, style) {
---
+++ src/main/scala/com/giyeok/passzero/ui/swt/SortedList.scala
@@ -0,0 +1,210 @@
+package com.giyeok.passzero.ui.swt
+
+import java.util.concurrent.atomic.AtomicLong
+import com.giyeok.passzero.utils.FutureStream
+import org.eclipse.swt.SWT
+import org.eclipse.swt.events.KeyEvent
+import org.eclipse.swt.events.KeyListener
+import org.eclipse.swt.events.MouseEvent
+import org.eclipse.swt.events.MouseListener
+import org.eclipse.swt.events.PaintEvent
+import org.eclipse.swt.events.PaintListener
+import org.eclipse.swt.graphics.Color
+import org.eclipse.swt.graphics.GC
+import org.eclipse.swt.graphics.Point
+import org.eclipse.swt.graphics.Rectangle
+import org.eclipse.swt.widgets.Canvas
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Display
+
+trait SortedListItem {
+    def >(other: SortedListItem): Boolean // = a > b
+    def dimension(gc: GC): Point
+    def draw(gc: GC, bounds: Rectangle, selected: Boolean): Unit
+}
+
+case class TextSortedListItem[T](data: T, text: String) extends SortedListItem {
+    def >(other: SortedListItem): Boolean = other match {
+        case otherItem: TextSortedListItem[_] => text < otherItem.text
+    }
+    def dimension(gc: GC): Point = {
+        gc.textExtent(text)
+    }
+    def draw(gc: GC, bounds: Rectangle, selected: Boolean): Unit = {
+        if (selected) {
+            gc.setBackground(SortedList.selectedBackgroundColor)
+        } else {
+            gc.setBackground(SortedList.baseBackgroundColor)
+        }
+        gc.drawText(text, bounds.x, bounds.y)
+    }
+}
+
+object SortedList {
+    val selectedBackgroundColor = new Color(null, 255, 255, 0)
+    val baseBackgroundColor = new Color(null, 255, 255, 255)
+}
+
+// TODO 나중에 StructuredTextView로 바꾸는 것을 고려
+class SortedList[T <: SortedListItem](display: Display, parent: Composite, style: Int) extends Canvas(parent, style) {
+    private var items = Seq[T]()
+    private var allDimension: Option[Point] = None
+    private var itemBoundsMap = Map[(T, Int), Rectangle]()
+
+    private var _selectedItem = Option.empty[Int]
+    def selectedItem: Option[(T, Int)] = _selectedItem map { idx => (items(idx), idx) }
+
+    private var _sourceId: Long = -1
+    private val sourceIdCounter = new AtomicLong(0)
+    private def newSourceId(): Long = {
+        val newId = sourceIdCounter.getAndIncrement()
+        this.synchronized { _sourceId = newId }
+        newId
+    }
+
+    private var _progress: Boolean = false
+    private def setProgress(progress: Boolean): Unit = {
+        _progress = progress
+        redraw()
+    }
+
+    private var _scroll: Point = new Point(0, 0)
+
+    private var _listeners: Seq[Option[(T, Int)] => Unit] = Seq()
+
+    setBackground(SortedList.baseBackgroundColor)
+
+    def clear(): Unit = {
+        items = Seq()
+        allDimension = None
+    }
+
+    def setSource(stream: FutureStream[Seq[T]]): Unit = this.synchronized {
+        clear()
+        setProgress(true)
+        val currentSourceId = newSourceId()
+        stream foreach { (page, tail) =>
+            this.synchronized {
+                if (this._sourceId == currentSourceId) {
+                    page foreach { item =>
+                        val index = items.zipWithIndex find { p => p._1 > item } map { _._2 } getOrElse items.length
+                        val (init, tail) = items.splitAt(index)
+                        val newList: Seq[T] = init ++ (item +: tail)
+                        _selectedItem match {
+                            case Some(idx) if idx >= index =>
+                                _selectedItem = Some(idx + 1)
+                            case _ => // do nothing
+                        }
+                        items = newList
+                        newList
+                    }
+                    allDimension = None // invalidate calculated dimension
+                    if (tail.isEmpty) {
+                        display.syncExec(() => { setProgress(false) })
+                    } else {
+                        display.syncExec(() => { redraw() })
+                    }
+                } else {
+                    // tail.cancel()
+                }
+            }
+        }
+    }
+
+    def addSelectListener(func: Option[(T, Int)] => Unit): Unit = {
+        _listeners +:= func
+    }
+
+    def select(item: Option[(T, Int)]): Unit = {
+        _selectedItem = item map { _._2 }
+        _listeners foreach { f => f(item) }
+        display.asyncExec(() => redraw())
+    }
+
+    def selectIndex(index: Option[Int]): Unit = {
+        _selectedItem = index
+        select(index map { idx => (items(idx), idx) })
+    }
+
+    addPaintListener(new PaintListener {
+        override def paintControl(e: PaintEvent): Unit = {
+            val gc = e.gc
+            if (allDimension.isEmpty) {
+                itemBoundsMap = Map()
+                val dimension = new Point(0, 0)
+                items.zipWithIndex foreach { itemIdx =>
+                    val (item, index) = itemIdx
+                    val d = item.dimension(gc)
+
+                    val itemBound = new Rectangle(0, dimension.y, d.x, d.y)
+                    itemBoundsMap += (itemIdx -> itemBound)
+
+                    dimension.x = math.max(dimension.x, d.x)
+                    dimension.y += d.y
+                }
+                allDimension = Some(dimension)
+            }
+            val contentDimension = allDimension.get
+            val bounds = getBounds
+            if (_scroll.x + bounds.width > contentDimension.x) {
+                _scroll.x = contentDimension.x - bounds.width
+            }
+            if (_scroll.x < 0) {
+                _scroll.x = 0
+            }
+            if (_scroll.y + bounds.height > contentDimension.y) {
+                _scroll.y = contentDimension.y - bounds.height
+            }
+            if (_scroll.y < 0) {
+                _scroll.y = 0
+            }
+
+            itemBoundsMap foreach { itemBound =>
+                val ((item, idx), bound) = itemBound
+
+                if (bound.y < bounds.height || (bound.y + bound.height) > bounds.y) {
+                    if (bound.x < bounds.width || (bound.x + bound.width) > bounds.x) {
+                        val isSelected = _selectedItem contains idx
+                        val scrolledBound = new Rectangle(bound.x - _scroll.x, bound.y - _scroll.y, bound.width, bound.height)
+                        item.draw(gc, scrolledBound, isSelected)
+                    }
+                }
+            }
+
+            if (_progress) {
+                gc.drawText("Loading...", 0, 0)
+            }
+        }
+    })
+
+    addMouseListener(new MouseListener {
+        override def mouseUp(e: MouseEvent): Unit = {}
+
+        override def mouseDoubleClick(e: MouseEvent): Unit = {}
+
+        override def mouseDown(e: MouseEvent): Unit = {
+            val p = new Point(e.x + _scroll.x, e.y + _scroll.y)
+            select(itemBoundsMap find { _._2.contains(p) } map { _._1 })
+        }
+    })
+
+    addKeyListener(new KeyListener {
+        override def keyPressed(e: KeyEvent): Unit = {
+            e.keyCode match {
+                case SWT.ARROW_DOWN =>
+                    _selectedItem match {
+                        case Some(idx) if idx + 1 < items.length => selectIndex(Some(idx + 1))
+                        case _ => // do nothing
+                    }
+                case SWT.ARROW_UP =>
+                    _selectedItem match {
+                        case Some(idx) if idx > 0 => selectIndex(Some(idx - 1))
+                        case _ => // do nothing
+                    }
+                case _ => // do nothing
+            }
+        }
+
+        override def keyReleased(e: KeyEvent): Unit = {}
+    })
+}
